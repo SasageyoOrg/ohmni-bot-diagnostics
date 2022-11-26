44f085738380addb267b14d15b6251e7692e5947
Andrian Melnic <andrianmelnic29@gmail.com>
2022-11-26 18:32:17 +0100

    imu_updater
    Added testing imu_updater node that checks angular velocities ranges and prints status messages in the /diagnostics monitor

---
+++ ros_ws/src/tb_sim/diagnostic_updater/include/diagnostic_updater/imu_updater.h
@@ -0,0 +1,66 @@
+#include <diagnostic_updater/diagnostic_updater.h>
+#include <std_msgs/Bool.h>
+#include <diagnostic_updater/publisher.h>
+#include <sensor_msgs/Imu.h> //This is a message to hold data from an IMU
+
+// ERROR defined in windows.h causes name collision, undefine the macro to fix the issue
+#ifdef ERROR
+#undef ERROR
+#endif
+using namespace diagnostic_updater;
+
+float av_x;
+float av_y;
+float av_z;
+float imu_av_min = 0.01;
+float imu_av_max = 1;
+
+
+// Callback function
+void imu_callback(const sensor_msgs::Imu::ConstPtr& msg)
+{
+  av_x = msg->angular_velocity.x;
+  av_y = msg->angular_velocity.y;
+  av_z = msg->angular_velocity.z;
+}
+
+/* Functions used to create DiagnosticTasks
+   Monitoring angular velocity values and checking if are out of range */ 
+
+void check_av_x(diagnostic_updater::DiagnosticStatusWrapper &stat)
+{  
+  if (av_x > imu_av_min && av_x < imu_av_max)
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "angular velocity x-axis: OK [%f]", av_x);
+  else if ( av_x > imu_av_max)
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::WARN, "angular velocity x-axis: Too high [%f]", av_x);
+  else 
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::WARN, "angular velocity x-axis: Too low [%f]", av_x);
+  stat.add("Top-Side Margin x", 10 - av_x);
+
+  stat.add("imu max", imu_av_max);
+  stat.add("imu min", imu_av_min);
+}
+
+void check_av_y(diagnostic_updater::DiagnosticStatusWrapper &stat)
+{  
+  if (av_y > imu_av_min && av_y < imu_av_max)
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "angular velocity y-axis: OK [%f]", av_y);
+  else if ( av_y > imu_av_max)
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::WARN, "angular velocity y-axis: Too high [%f]", av_y);
+  else 
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::WARN, "angular velocity y-axis: Too low [%f]", av_y);
+  stat.add("Top-Side Margin y", 10 - av_y);
+}
+
+void check_av_z(diagnostic_updater::DiagnosticStatusWrapper &stat)
+{  
+  if (av_z > imu_av_min && av_z < imu_av_max)
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "angular velocity z-axis: OK [%f]", av_z);
+  else if ( av_z > imu_av_max)
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::WARN, "angular velocity z-axis: Too high [%f]", av_z);
+  else 
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::WARN, "angular velocity z-axis: Too low [%f]", av_z);
+  stat.add("Top-Side Margin z", 10 - av_z);
+}
+
+
---
+++ ros_ws/src/tb_sim/diagnostic_updater/src/imu_updater.cpp
@@ -0,0 +1,55 @@
+#include <diagnostic_updater/imu_updater.h>
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "imu_updater"); //Ros initialization function.
+  ros::NodeHandle nh; //Construct a NodeHandle Class. This class is used for writing nodes. 
+  
+  diagnostic_updater::Updater imuUpdater; //Construct an updater class.
+
+  imuUpdater.setHardwareID("/tb_sim/imu");
+
+  /* Subscribing to /tb_sim/imu topic
+     Message queue = 1000
+     Callback function = imu_callback
+  */
+  ros::Subscriber subImu = nh.subscribe("/tb_sim/imu", 1000, imu_callback);
+
+  // Creating tasks using functions with FunctionDiagnosticTask
+  diagnostic_updater::FunctionDiagnosticTask av_x("Chek av x",
+       boost::bind(&check_av_x, boost::placeholders::_1));
+
+  diagnostic_updater::FunctionDiagnosticTask av_y("Chek av y",
+       boost::bind(&check_av_y, boost::placeholders::_1));
+
+  diagnostic_updater::FunctionDiagnosticTask av_z("Chek av z",
+       boost::bind(&check_av_z, boost::placeholders::_1));
+       
+  diagnostic_updater::CompositeDiagnosticTask bounds("Angual velocity bounds check");
+
+  //Creates a new task, registers the task, and returns the instance.
+  bounds.addTask(&av_x);
+  bounds.addTask(&av_y);
+  bounds.addTask(&av_z);
+  //Add the CompositeDiagnosticTask to our Updater.
+  imuUpdater.add(bounds);
+
+
+  imuUpdater.broadcast(0, "Initializing IMU updater");
+ 
+  imuUpdater.force_update();
+ 
+
+  while (nh.ok())
+  {
+    
+    ros::Duration(0.1).sleep();
+    //spinOnce() will call all the callbacks waiting to be called at that point in time.
+    ros::spinOnce();
+    //Call updater
+    imuUpdater.update();
+    
+  }
+
+  return 0; 
+}
