4986e63ba543c5d8a41e3d224bb0afd36726ce98
Andrian Melnic <andrianmelnic29@gmail.com>
2022-11-28 12:22:43 +0100

    imu_updater
    Tested new tasks creation by adding linear acceleration bound checking

--- ros_ws/src/tb_sim/diagnostic_updater/include/diagnostic_updater/imu_updater.h
+++ ros_ws/src/tb_sim/diagnostic_updater/include/diagnostic_updater/imu_updater.h
@@ -10,16 +10,28 @@ using namespace diagnostic_updater;
 float av_x;
 float av_y;
 float av_z;
+
+float la_x;
+float la_y;
+float la_z;
+
 float imu_av_min;
 float imu_av_max;
 
+float imu_la_min;
+float imu_la_max;
 
-// Callback function, runed
+
+// Callback function
 void imu_callback(const sensor_msgs::Imu::ConstPtr& msg)
 {
   av_x = msg->angular_velocity.x;
   av_y = msg->angular_velocity.y;
   av_z = msg->angular_velocity.z;
+
+  la_x = msg->linear_acceleration.x;
+  la_y = msg->linear_acceleration.y;
+  la_z = msg->linear_acceleration.z;
 }
 
 /* 
@@ -38,8 +50,8 @@ void check_av_x(diagnostic_updater::DiagnosticStatusWrapper &stat)
     stat.summaryf(diagnostic_msgs::DiagnosticStatus::WARN, "angular velocity x-axis: Too low [%f]", av_x);
   stat.add("Top-Side Margin x", 10 - av_x);
 
-  stat.add("imu max", imu_av_max);
-  stat.add("imu min", imu_av_min);
+  stat.add("imu la max", imu_av_max);
+  stat.add("imu la min", imu_av_min);
 }
 
 void check_av_y(diagnostic_updater::DiagnosticStatusWrapper &stat)
@@ -64,4 +76,39 @@ void check_av_z(diagnostic_updater::DiagnosticStatusWrapper &stat)
   stat.add("Top-Side Margin z", 10 - av_z);
 }
 
+// Linear acceleration
+void check_la_x(diagnostic_updater::DiagnosticStatusWrapper &stat)
+{  
+  if (la_x > imu_la_min && la_x < imu_la_max)
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "linear acceleration x-axis: OK [%f]", la_x);
+  else if ( la_x > imu_la_max)
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::WARN, "linear acceleration x-axis: Too high [%f]", la_x);
+  else 
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::WARN, "linear acceleration x-axis: Too low [%f]", la_x);
+  stat.add("Top-Side Margin x", 10 - la_x);
 
+  stat.add("imu la max", imu_la_max);
+  stat.add("imu la min", imu_la_min);
+}
+
+void check_la_y(diagnostic_updater::DiagnosticStatusWrapper &stat)
+{  
+  if (la_y > imu_la_min && la_y < imu_la_max)
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "linear acceleration y-axis: OK [%f]", la_y);
+  else if ( la_y > imu_la_max)
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::WARN, "linear acceleration y-axis: Too high [%f]", la_y);
+  else 
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::WARN, "linear acceleration y-axis: Too low [%f]", la_y);
+  stat.add("Top-Side Margin y", 10 - la_y);
+}
+
+void check_la_z(diagnostic_updater::DiagnosticStatusWrapper &stat)
+{  
+  if (la_z > imu_la_min && la_z < imu_la_max)
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "linear acceleration z-axis: OK [%f]", la_z);
+  else if ( la_z > imu_la_max)
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::WARN, "linear acceleration z-axis: Too high [%f]", la_z);
+  else 
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::WARN, "linear acceleration z-axis: Too low [%f]", la_z);
+  stat.add("Top-Side Margin z", 10 - la_z);
+}
--- ros_ws/src/tb_sim/diagnostic_updater/src/imu_updater.cpp
+++ ros_ws/src/tb_sim/diagnostic_updater/src/imu_updater.cpp
@@ -9,6 +9,9 @@ int main(int argc, char **argv)
   nh.getParam("imu_av_min", imu_av_min); // Get parameters from .yaml file 
   nh.getParam("imu_av_max", imu_av_max);
 
+  nh.getParam("imu_la_min", imu_la_min);
+  nh.getParam("imu_la_max", imu_la_max);
+
 	imuUpdater.setHardwareID("/tb_sim/imu");
 
   /* Subscribing to /tb_sim/imu topic
@@ -20,22 +23,36 @@ int main(int argc, char **argv)
 
   // Creating tasks using functions with FunctionDiagnosticTask
   // 
-  diagnostic_updater::FunctionDiagnosticTask av_x("Chek av x",
+  // Angular velocity
+  diagnostic_updater::FunctionDiagnosticTask av_x("Check av x",
     boost::bind(&check_av_x, boost::placeholders::_1));
 
-  diagnostic_updater::FunctionDiagnosticTask av_y("Chek av y",
+  diagnostic_updater::FunctionDiagnosticTask av_y("Check av y",
     boost::bind(&check_av_y, boost::placeholders::_1));
 
-  diagnostic_updater::FunctionDiagnosticTask av_z("Chek av z",
+  diagnostic_updater::FunctionDiagnosticTask av_z("Check av z",
     boost::bind(&check_av_z, boost::placeholders::_1));
 
+// Linear acceleration
+  diagnostic_updater::FunctionDiagnosticTask la_x("Check la x",
+    boost::bind(&check_la_x, boost::placeholders::_1));
+
+  diagnostic_updater::FunctionDiagnosticTask la_y("Check la y",
+    boost::bind(&check_la_y, boost::placeholders::_1));
+
+  diagnostic_updater::FunctionDiagnosticTask la_z("Check la z",
+    boost::bind(&check_la_z, boost::placeholders::_1));
     
-  diagnostic_updater::CompositeDiagnosticTask bounds("Angual velocity bounds check");
+  diagnostic_updater::CompositeDiagnosticTask bounds("IMU bounds check");
 
   //Creates a new task, registers the task, and returns the instance.
   bounds.addTask(&av_x);
   bounds.addTask(&av_y);
   bounds.addTask(&av_z);
+
+  bounds.addTask(&la_x);
+  bounds.addTask(&la_y);
+  bounds.addTask(&la_z);
   //Add the CompositeDiagnosticTask to our Updater.
   imuUpdater.add(bounds);
 
