7fc4337e5df480e036886f3d62c2ff66ee143da6
Andrian Melnic <andrianmelnic29@gmail.com>
2022-12-05 11:13:59 +0100

    Revert "switchOff-jutsu"
    This reverts commit d1848dc226c0e4771cfbf367d3ee08fdeddeb7d1.

--- ros_ws/src/tb_sim/diagnostic_updater/include/diagnostic_updater/navcam_updater.h
+++ ros_ws/src/tb_sim/diagnostic_updater/include/diagnostic_updater/navcam_updater.h
@@ -3,20 +3,13 @@
 #include <diagnostic_updater/publisher.h>
 #include <sensor_msgs/Image.h> 
 #include <math.h>
-#include <gazebo/sensors/SensorManager.hh>
-#include <gazebo/sensors/Sensor.hh>
-
 #ifdef ERROR
 #undef ERROR
 #endif
 using namespace diagnostic_updater;
-using namespace gazebo;
 
 int seq;
 int navcam_seq_old = 0;
-
-sensors::SensorPtr sensor = sensors::SensorManager::Instance()->GetSensor(
-    "default::tb_sim::base_link::nav_camera");
 
 // Callback function
 void navcam_callback(const sensor_msgs::ImageConstPtr& msg)
@@ -27,10 +20,6 @@ void navcam_callback(const sensor_msgs::ImageConstPtr& msg)
 
 void check_seq(diagnostic_updater::DiagnosticStatusWrapper &stat)
 { 
-
-    if (seq == 100)
-        sensor->SetActive(false);
-    
     if(seq == navcam_seq_old) 
         stat.summaryf(diagnostic_msgs::DiagnosticStatus::WARN, "CAMERA OFF");
     else
@@ -40,4 +29,6 @@ void check_seq(diagnostic_updater::DiagnosticStatusWrapper &stat)
     navcam_seq_old = seq;
     stat.add("Seq", seq);
 }
+
+
 
