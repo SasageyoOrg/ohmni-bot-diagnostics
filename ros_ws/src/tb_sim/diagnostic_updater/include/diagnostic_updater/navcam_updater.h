b12a2b1d6148e8858f3e6eb400f2971b24868eec
Andrian Melnic <andrianmelnic29@gmail.com>
2022-12-06 21:41:39 +0100

    clean-jutsu

--- ros_ws/src/tb_sim/diagnostic_updater/include/diagnostic_updater/navcam_updater.h
+++ ros_ws/src/tb_sim/diagnostic_updater/include/diagnostic_updater/navcam_updater.h
@@ -2,7 +2,6 @@
 #include <std_msgs/Bool.h>
 #include <diagnostic_updater/publisher.h>
 #include <sensor_msgs/Image.h> 
-#include <math.h>
 #ifdef ERROR
 #undef ERROR
 #endif
@@ -14,20 +13,19 @@ int navcam_seq_old = 0;
 // Callback function
 void navcam_callback(const sensor_msgs::ImageConstPtr& msg)
 {
-    // seq = msg->header.seq;
     seq = msg->header.seq;
 }
 
 void check_seq(diagnostic_updater::DiagnosticStatusWrapper &stat)
 { 
+    stat.add("Seq prev", navcam_seq_old);
+    stat.add("Seq", seq);
     if(seq == navcam_seq_old) 
         stat.summaryf(diagnostic_msgs::DiagnosticStatus::WARN, "CAMERA OFF");
     else
         stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "CAMERA ON");
 
-    stat.add("Seq prev", navcam_seq_old);
     navcam_seq_old = seq;
-    stat.add("Seq", seq);
 }
 
 
--- ros_ws/src/tb_sim/diagnostic_updater/include/diagnostic_updater/telecam_updater.h
+++ ros_ws/src/tb_sim/diagnostic_updater/include/diagnostic_updater/telecam_updater.h
@@ -2,7 +2,6 @@
 #include <std_msgs/Bool.h>
 #include <diagnostic_updater/publisher.h>
 #include <sensor_msgs/Image.h> 
-#include <math.h>
 #ifdef ERROR
 #undef ERROR
 #endif
@@ -14,20 +13,19 @@ int telecam_seq_old = 0;
 // Callback function
 void telecam_callback(const sensor_msgs::ImageConstPtr& msg)
 {
-    // seq = msg->header.seq;
     seq = msg->header.seq;
 }
 
 void check_seq(diagnostic_updater::DiagnosticStatusWrapper &stat)
 { 
+    stat.add("Seq prev", telecam_seq_old);
+    stat.add("Seq", seq);
     if(seq == telecam_seq_old) 
         stat.summaryf(diagnostic_msgs::DiagnosticStatus::WARN, "CAMERA OFF");
     else
         stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "CAMERA ON");
 
-    stat.add("Seq prev", telecam_seq_old);
     telecam_seq_old = seq;
-    stat.add("Seq", seq);
 }
 
 
